6187e5aad18af9b1895c392b9b609dd83c69b931
Yana Khuchuieva <yana.khuchuieva@maersk.com>
2022-12-14 18:40:34 +0200

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Robot } from './components/Card';
 import { CardList } from './components/CardList';
 import { SearchBox } from './components/SearchBox';
-import { robots } from './initial/robots';
 
 function App() {
 
-  const [searchInput, changeSearchInput] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [robots, setRobots] = useState<Robot[]>([]);
+
+useEffect(() => {
+  (async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users');
+    const robots = await data.json()
+    setRobots(robots)
+  })()
+}, [])
 
   const filteredRobots =  robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchInput.toLowerCase());
+    return robot.name.toLowerCase().includes(inputValue.toLowerCase());
   })
   
   return (  
-    <div className="App">
-      <SearchBox searchHandler={changeSearchInput}/>
+    <div className="tc">
+      <SearchBox searchHandler={setInputValue}/>
       <CardList robots={filteredRobots}/>
     </div>
   );
